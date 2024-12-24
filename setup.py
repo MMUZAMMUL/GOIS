605faa376151eeba33321ba1b341db2e0440172f
MMUZAMMUL <121809247+MMUZAMMUL@users.noreply.github.com>
2024-12-24 13:50:48 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@ from setuptools import setup, find_packages
 setup(
     name="gois",
     version="0.1.0",
-    description="A package for GOIS-based inference and evaluation",
+    description="A package for GOIS-based inference, evaluation, and preprocessing",
     author="MUHAMMAD MUZAMMUL",
     author_email="munagreat123@gmail.com",
     url="https://github.com/MMUZAMMUL/GOIS",
-    packages=find_packages(),  # Automatically finds the 'my_package' package
-    include_package_data=True,
+    packages=find_packages(include=["my_package", "my_package.*"]),  # Include the `my_package` directory
+    include_package_data=True,  # Include non-Python files specified in MANIFEST.in
     install_requires=[
         "ultralytics>=8.0.0",
         "gdown>=4.5.1",
@@ -16,16 +16,17 @@ setup(
         "numpy>=1.21.6",
         "Pillow>=9.0.1",
         "torch>=1.10.0",
-    
+        "thop @ git+https://github.com/Lyken17/pytorch-OpCounter.git",
     ],
     entry_points={
         "console_scripts": [
-            "gois-download-data=my_package.download_data:main",
-            "gois-download-models=models.download_models:download_ultralytics_models",
-            "gois-full-inference=scripts.full_inference:main",
-            "gois-gois-inference=scripts.gois_inference:main",
+            "gois-download-data=data.download_data:main",
+            "gois-download-models=Models.download_models:main",
+            "gois-full-inference=my_package.full_inference:main",
+            "gois-gois-inference=my_package.gois_inference:main",
             "gois-evaluate-full=scripts.evaluate_full_inference:main",
             "gois-evaluate-gois=scripts.evaluate_gois:main",
+            "gois-generate-ground-truth=scripts.generate_ground_truth:main",
         ]
     },
     classifiers=[
